7a50e90bdeb630b3e42bdbb2f750aaa9424e6490
amanc1248 <amanc1248@gmail.com>
2022-05-23 14:57:20 +0545

    CONNECTED: metamask wallet

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -16,17 +16,39 @@ const getEthereumContract = ()=>{
 }
 
 export const TransactionProvider = ({children})=>{
+    const [currentAccount, setCurrentAccount] = useState([])
     const checkIfWalletIsConnected = async ()=>{
-        if(!ethereum) return alert("Please install metamask");
+        try {
+            if(!ethereum) return alert("Please install metamask");
         const accounts =await ethereum.request({method: 'eth_accounts'});
+        if(accounts.length){
+            setCurrentAccount(accounts[0])
+        }else{
+            console.log("no account found")
+        }
         console.log(accounts);
+        } catch (error) {
+            console.log(error);
+            throw new Error('no ethereum object')
+        }
+    }
+
+    const connectWallet  =async()=>{
+        try {
+            if(!ethereum) return alert("Please install metamask");
+            const accounts =await ethereum.request({method: 'eth_requestAccounts'});
+            setCurrentAccount(accounts[0]);
+        } catch (error) {
+            console.log(error);
+            throw new Error('no ethereum object')
+        }
     }
     useEffect(() => {
       checkIfWalletIsConnected();
     }, [])
     
     return(
-        <TransactionContext.Provider value={{value:"test"}}>
+        <TransactionContext.Provider value={{connectWallet,currentAccount}}>
             {children}
         </TransactionContext.Provider>
     )
